fe90b7cfce4968b47daf7807c6c184db8b9c5e5e
Yosoi <yosoi@protonmail.com>
2020-07-05 12:41:38 -0400

    goal effect

--- src/game/objects/goal/effects/Shockwave.js
+++ src/game/objects/goal/effects/Shockwave.js
@@ -1,3 +1,20 @@
 export default function(scene, x, y) {
-  const circle = scene.add.circle(x, y, 100, 0x000000);
+  const camera = scene.cameras.main;
+  camera.flash();
+  camera.shake();
+  // stop time and then bring it back to normal speed
+  const circle = scene.add.circle(x, y, 1, 0xF9C80E);
+  circle.setDepth(-1);
+  scene.tweens.add({
+    targets: circle,
+    duration: 200,
+    ease: "Bounce",
+    alpha: {from: 1, to: 0},
+    radius: {from: 1, to: 500},
+    onComplete: () => circle.destroy()
+  })
+  // set circle underneath everything else
+  // set gradient fill
+  // animate gradient
+  // destroy circle on animation finished
 }
