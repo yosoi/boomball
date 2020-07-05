e84018c123498d35be6040e9bed50d5adb2b9ec7
Yosoi <yosoi@protonmail.com>
2020-07-05 11:57:21 -0400

    rename graphics to effects. create boilerplate for goal shockwave

--- src/game/objects/goal/Goal.js
+++ src/game/objects/goal/Goal.js
@@ -1,9 +1,13 @@
 import Net from './Net'
 import Phaser from 'phaser'
+import Shockwave from './effects/Shockwave'
 
 export default function(scene, x, height, onScoredUpon, startTop = true) {
   scene.add.rectangle(x, 300, 1, height, 0xDDDDDD);
-  const net = new Net(scene, x, onScoredUpon);
+  const net = new Net(scene, x, () => {
+    new Shockwave(this);
+    onScoredUpon();
+  });
   const start = startTop ? 200 : 400;
   const stop = startTop ? 400 : 200;
   scene.tweens.add({
